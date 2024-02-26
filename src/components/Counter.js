0b1e7b3c68848d8f235f129b86b379f3c21b0ba7
kDera <caduvazquezabj@gmail.com>
2024-02-26 15:49:40 -0300

    last save

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,22 +1,35 @@
 import { Component } from "react";
 
 class Counter extends Component{
-    constructor(){
+    constructor(){ // construtor para uso do estado da variável
         super();
         this.state ={
-            counter: 0,
+            counter: 0, //valor inicial
         };
     }
+
+    increment(){//somador simples
+        this.setState({ //mudança de eestado da variável
+            counter: this.state.counter + 1
+        })
+    }
+
+    decrement(){//subtrator simples
+        this.setState({
+            counter: this.state.counter - 1
+        })
+    }
+
     render(){
         return(
         <div>
-            <h3>Counter value is: {this.state.counter} </h3>
-            <button>Click</button>
+            <h3>Counter value is: {this.state.counter} </h3> 
+            <button onClick={()=>this.increment()}>Increment</button>
+            <button onClick={()=>this.decrement()}>Decrement</button>
         </div>
-        )
-
-        
+        )   
     }
+
 }
 
 export default Counter;
